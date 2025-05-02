09362937777fec3465df1e416d8417904d441e12
Todd Wang <wenying@TodddeMacBook-Pro.local>
2025-05-02 13:55:08 +0800

    [feat]add CICD

--- src/main/java/com/example/employee_attendance_leave_system/EmployeeAttendanceLeaveSystemApplication.java
+++ src/main/java/com/example/employee_attendance_leave_system/EmployeeAttendanceLeaveSystemApplication.java
@@ -2,12 +2,19 @@ package com.example.employee_attendance_leave_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 
 @SpringBootApplication
 public class EmployeeAttendanceLeaveSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeAttendanceLeaveSystemApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EmployeeAttendanceLeaveSystemApplication.class);
 	}
 
 }
